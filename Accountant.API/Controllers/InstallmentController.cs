163ecaaf510059eacb0dab6e6f27ae6d1d280dc4
Hamid <gmhamid6@gmail.com>
2024-08-06 09:58:46 +0330

    the Api for Loan and Installments is evolved

--- Accountant.API/Controllers/InstallmentController.cs
+++ Accountant.API/Controllers/InstallmentController.cs
@@ -13,15 +13,13 @@ namespace Accountant.API.Controllers
     {
         private readonly IinstallmentRepository _repository;
         private readonly ILoanRepository _loanRepository;
-        private readonly InstallmentBusiness _business;
 
         public InstallmentController(IinstallmentRepository repository,
-                                     ILoanRepository loanRepository,
-                                     InstallmentBusiness business)
+                                     ILoanRepository loanRepository)
         {
             _repository = repository;
             _loanRepository = loanRepository;
-            _business = business;
+
         }
 
         [HttpGet(Name = "GetInstallments")]
@@ -86,77 +84,6 @@ namespace Accountant.API.Controllers
             }
         }
 
-        //[HttpPost("{loan}", Name = "AddNewInstallment")]
-        //[ProducesResponseType(200)]
-        //[ProducesResponseType(400)]
-        //[ProducesResponseType(404)]
-        //public async Task<ActionResult<Installment>> AddNewInstallment(LoanDto loan)
-        //{
-        //    try
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            if (await _loanRepository.ExistLoan(loan.ID))
-        //            {
-        //                foreach (var item in await _business.AddInstallments(loan))
-        //                {
-        //                    var addInstallment = await _repository.AddNewInstallments(item);
-
-        //                    if (!addInstallment)
-        //                        return BadRequest();
-        //                }
-
-        //                return Ok("successfully");
-        //            }
-        //            else
-        //            {
-        //                return NotFound();
-        //            }
-        //        }
-        //        else
-        //        {
-        //            return BadRequest(ModelState);
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        return BadRequest();
-        //    }
-        //}
-
-        //[HttpPut(Name = "UpdateInstallments")]
-        //[ProducesResponseType(200)]
-        //[ProducesResponseType(400)]
-        //[ProducesResponseType(404)]
-        //public async Task<ActionResult<ICollection<Installment>>> UpdateInstallments(LoanDto loan)
-        //{
-        //    try
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            var installmentsUpdate = await _business.UpdateInstallment(loan);
-
-        //            if (installmentsUpdate != null)
-        //            {
-        //                var UpdateResponse = await _repository
-        //                return Ok("successfully");
-        //            }
-        //            else
-        //            {
-        //                return NotFound();
-        //            }
-        //        }
-        //        else
-        //        {
-        //            return BadRequest(ModelState);
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        return BadRequest();
-        //    }
-        //}
-
         [HttpPatch("{InstallmentID:int}", Name = "PayInstallment")]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
@@ -186,24 +113,24 @@ namespace Accountant.API.Controllers
             }
         }
 
-        [HttpDelete("{LoanID:int}", Name = "DeleteInstallmentsByLoanID")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public async Task<ActionResult<bool>> DeleteInstallmentsByLoanID(int LoanID)
-        {
-            try
-            {
-                var Delete = await _repository.RemoveInstallments(LoanID);
-                if (Delete)
-                    return Ok("successfully");
-                else
-                    return NotFound();
-            }
-            catch
-            {
-                return BadRequest();
-            }
-        }
+        //[HttpDelete("{LoanID:int}", Name = "DeleteInstallmentsByLoanID")]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+        //public async Task<ActionResult<bool>> DeleteInstallmentsByLoanID(int LoanID)
+        //{
+        //    try
+        //    {
+        //        var Delete = await _repository.RemoveInstallments(LoanID);
+        //        if (Delete)
+        //            return Ok("successfully");
+        //        else
+        //            return NotFound();
+        //    }
+        //    catch
+        //    {
+        //        return BadRequest();
+        //    }
+        //}
     }
 }

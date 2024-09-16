303f9c267ae8925ea495816166c68bf61d7da8c5
Hamid <gmhamid6@gmail.com>
2024-09-16 11:27:48 +0330

    disable sign up button after click

--- Accountant.Web/Pages/UserPages/SignUpBase.cs
+++ Accountant.Web/Pages/UserPages/SignUpBase.cs
@@ -13,16 +13,17 @@ namespace Accountant.Web.Pages.UserPages
         public NavigationManager navigationManager { get; set; }
         [Inject]
         public IJSRuntime JS { get; set; }
-       
+
         public UserDto? NewUser { get; set; }
         public int userid { get; set; }
-        public string? Username { get; set; } 
+        public string? Username { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
         public string? Email { get; set; }
         public string? ImageURL { get; set; }
+        public bool IsDisabled { get; set; }
 
-        public string? ErorMessage { get; set; }
+        public string? ErrorMessage { get; set; }
 
 
         protected async void AddNewUser_Click()
@@ -34,6 +35,7 @@ namespace Accountant.Web.Pages.UserPages
 
                     if (Username != null && Password != null && Email != null)
                     {
+                        IsDisabled = true;
 
                         NewUser = new UserDto
                         {
@@ -42,18 +44,18 @@ namespace Accountant.Web.Pages.UserPages
                             UserName = Username,
                             ImgURL = ImageURL
                         };
-
                         if (await UserServices.SignUp(NewUser) != null)
                         {
                             await JS.InvokeAsync<UserDto>("alert", "You Signup With success ! ");
-                            StateHasChanged();
 
-                            navigationManager.NavigateTo($"/UserMainPage/{Username}/{Password}",true);
+                            navigationManager.NavigateTo($"/UserMainPage/{Username}/{Password}", true);
                         }
 
                         else
                         {
-                            await JS.InvokeVoidAsync("alert", "Somthing went wrong while signup you ! ");
+                            await JS.InvokeVoidAsync("alert", "Something went wrong while signup you ! ");
+                            IsDisabled = false;
+                            StateHasChanged();
                         }
                     }
 
@@ -65,16 +67,18 @@ namespace Accountant.Web.Pages.UserPages
 
                 else
                 {
-                   await JS.InvokeAsync<UserDto>("alert", "Your password and confirm should be match exactly !");
+                    await JS.InvokeAsync<UserDto>("alert", "Your password and confirm should be match exactly !");
                 }
             }
 
             catch (Exception ex)
             {
-                await JS.InvokeVoidAsync("alert", "Somthing went wrong while signup you maybe your username taken before ! ");
-
-                ErorMessage = ex.Message;
+                IsDisabled = false;
+                StateHasChanged();
+                await JS.InvokeVoidAsync("alert", "Something went wrong while signup you maybe your username taken before ! ");
+                ErrorMessage = ex.Message;
             }
+
         }
 
     }

d3344c833abaf42d646a4c8d55c358b9c254d95b
Hamid <gmhamid6@gmail.com>
2025-07-18 16:51:12 +0330

    project api's compeleted and starting the web

---
+++ Accountant.Web/Pages/AdminPages/AdminControlPageBase.cs
@@ -0,0 +1,25 @@
+﻿using Accountant.Web.Services.Contract;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Accountant.Web.Pages.AdminPages
+{
+    public class AdminControlPageBase : ComponentBase
+    {
+        [Parameter]
+        public string AdminSpec { get; set; }
+        [Parameter]
+        public string Password { get; set; }
+
+        [Inject]
+        public IAdminServices AdminServices { get; set; }
+        [Inject]
+        public IJSRuntime js { get; set; }
+
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
